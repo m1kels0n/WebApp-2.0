224f834b4578079ce5a9e06606ff3e92ff9cfd66
Michael <michaelbserrano@gmail.com>
2025-07-18 22:48:24 +0800

    update auth

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebaseConfig.initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
